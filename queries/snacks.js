c5c018084dda1eb9771f294bcc160e06c52b7f12
NRiddick113 <NatashaRiddick@pursuit.org>
2023-02-02 09:46:09 -0500

    update price

--- queries/snacks.js
+++ queries/snacks.js
@@ -26,14 +26,14 @@ const getAsnack = async (id) => {
 const createSnacks = async (snack) => {
   try {
     const createSnack = await db.one(
-      "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy,amount, image) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy,price, image) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
       [
         snack.name,
         snack.fiber,
         snack.protein,
         snack.added_sugar,
         snack.is_healthy,
-        snack.amount,
+        snack.price,
         snack.image,
       ]
     );
@@ -49,13 +49,13 @@ const updateSnacks = async (id, snack) => {
   //Protein was spelled incorrectly as"protien"
   try {
     const updateSnack = await db.one(
-      "UPDATE snacks SET (name=$1, fiber=$2, protein=$3, added_sugar=$4, is_healthy=$5, amount=$6, image=$7) WHERE id=$8 RETURNING *",
+      "UPDATE snacks SET (name=$1, fiber=$2, protein=$3, added_sugar=$4, is_healthy=$5, price=$6, image=$7) WHERE id=$8 RETURNING *",
       [
         snack.name,
         snack.fiber,
         snack.protein,
         snack.is_healthy,
-        snack.amount,
+        snack.price,
         snack.image,
         id,
       ]
